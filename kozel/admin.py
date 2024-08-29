b36ba470f84f17b22799038cecb43c9245051d63
mrgarcia-uens-ec <jesus.garcia@uens.edu.ec>
2024-08-29 07:52:53 +0000

    Clase 2024-08-28

--- kozel/admin.py
+++ kozel/admin.py
@@ -3,7 +3,21 @@ from django.contrib import admin
 from .models import Estudiante
 from .models import Curso
 from .models import Asignatura
+from .models import TipoArticulo
+from .models import Articulo
+from .models import Variedad
+from .models import Carrito
+from .models import CarritoVariedad
+from .models import Compra
+from .models import CompraVariedad
 
 admin.site.register(Estudiante)
 admin.site.register(Asignatura)
 admin.site.register(Curso)
+admin.site.register(TipoArticulo)
+admin.site.register(Articulo)
+admin.site.register(Variedad)
+admin.site.register(Carrito)
+admin.site.register(CarritoVariedad)
+admin.site.register(Compra)
+admin.site.register(CompraVariedad)
--- kozel/models.py
+++ kozel/models.py
@@ -1,6 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class TipoArticulo(models.Model):
+   codigo = models.CharField(max_length=50, null=True) 
+   nombre = models.CharField(max_length=100, null=True)
+
+class Articulo(models.Model):
+   nombre = models.CharField(max_length=100, null=True)
+   descripcion = models.CharField(max_length=500, null=True)
+   foto1 = models.CharField(max_length=200, null=True)
+   foto2 = models.CharField(max_length=200, null=True)
+   tipoArticulo = models.ForeignKey(TipoArticulo, on_delete=models.DO_NOTHING, null=True)
+
+class Variedad(models.Model):
+   talla = models.CharField(max_length=10, null=True)
+   color = models.CharField(max_length=20, null=True)
+   precio = models.DecimalField
+   stock = models.PositiveIntegerField(default=0)
+   producto = models.ForeignKey(Articulo, on_delete=models.DO_NOTHING, null=True)
+
+class Carrito(models.Model):
+    fechaCreacion = models.DateTimeField
+    usuario = models.CharField(max_length=100, null=True)
+
+class CarritoVariedad(models.Model):
+    cantidad = models.DecimalField
+    carrito = models.ForeignKey(Carrito, on_delete=models.DO_NOTHING, null=True)
+    variedad = models.ForeignKey(Variedad, on_delete=models.DO_NOTHING, null=True)
+
+class Compra(models.Model):
+    fechaCreacion = models.DateTimeField
+    usuario = models.CharField(max_length=100, null=True)
+
+class CompraVariedad(models.Model):
+    cantidad = models.DecimalField
+    compra = models.ForeignKey(Compra, on_delete=models.DO_NOTHING, null=True)
+    variedad = models.ForeignKey(Variedad, on_delete=models.DO_NOTHING, null=True)
+
 class Curso(models.Model):
     def __str__(self):
         return self.nombre
